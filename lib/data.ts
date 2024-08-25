cbe10642636ee893176c2e3a8115baad320b909c
muffafa <mm.savar@gmail.com>
2024-08-26 00:15:15 +0300

    [update]: work experince added and all descriptions updated

--- lib/data.ts
+++ lib/data.ts
@@ -34,10 +34,17 @@ export const links = [
 
 export const experiencesData = [
   {
+    title: "Borda Tech - Product Manager",
+    location: "Istanbul, Turkey (Intern)",
+    description:
+      "During my internship at Borda Tech, I played a crucial role in the development of an AI-powered assistant for their healthcare product, Quattro. I gained hands-on experience in product management, including defining product vision, conducting competitor analysis, and engaging in cross-functional collaboration. My work involved stakeholder management, roadmapping, and establishing KPIs, ensuring a comprehensive approach to product development. I also developed a deep understanding of IoT applications in healthcare, software testing, and UX design, which enhanced my ability to contribute effectively to the project.",
+    icon: React.createElement(CgWorkAlt),
+    date: "Jul 2024 - Present",
+  },
+  {
     title: "Kodluyoruz - Associate Team Member",
     location: "İstanbul, Turkey (Full Time - Remote)",
-    description:
-      "Kodluyoruz is a social enterprise. I am contributing to technological education and development.",
+    description: `During my tenure at Kodluyoruz, I played a key role in planning and managing free software training programs for youth, leading the design and implementation of events and curricula. I ensured effective communication and follow-up with participants, contributing to their job placement process and certificate distribution. I also enhanced productivity through tools like Google Docs and Drive and managed technical aspects of face-to-face events. I actively participated in significant collaborations with organizations such as IBM and Microsoft, and led projects like "Kodluyoruz Açık Kaynak" and "Kodluyoruz Harekete Geç," demonstrating my ability to handle both strategic and technical responsibilities effectively.`,
     icon: React.createElement(CgWorkAlt),
     date: "Jul 2023 - Present",
   },
@@ -45,7 +52,7 @@ export const experiencesData = [
     title: "Zencir Software - Team Lead",
     location: "Adana, Turkey (Freelance)",
     description:
-      "I lead our online services and work closely with clients through popular freelancer websites such as Bionluk. We specialize in web design and corporate identity services.",
+      "As the Founder and Project Manager of Zencir Software, I built and led a freelance team that specialized in creating customized websites tailored to clients' needs. I managed the full project lifecycle, from analyzing client requirements to delivering projects on time and within budget. My focus on maintaining high customer satisfaction and continuously improving service quality through feedback contributed to the success of our projects. I demonstrated expertise across various web and design technologies, ensuring that our solutions effectively supported clients' corporate identities.",
     icon: React.createElement(FaReact),
     date: "Apr 2022 - Jan 2023",
   },
