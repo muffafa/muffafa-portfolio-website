1c2daa9acb4dbd69c39caf9986e59d318e460ac9
muffafa <mm.savar@gmail.com>
2024-08-26 00:24:03 +0300

    [update]: resume updated

--- components/intro.tsx
+++ components/intro.tsx
@@ -96,7 +96,7 @@ export default function Intro() {
 
         <a
           className="group bg-white px-7 py-3 flex items-center gap-2 rounded-full outline-none focus:scale-110 hover:scale-110 active:scale-105 transition cursor-pointer borderBlack dark:bg-white/10"
-          href="/online-resume-muhammed-mustafa-savar.pdf"
+          href="/online-resume-muhammed-mustafa-savar_25.08.2024.pdf"
           download
         >
           Download CV{" "}
