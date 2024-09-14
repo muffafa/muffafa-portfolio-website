50bb1e4968ecc29ac0f56a2d57d4a33aa2bfc117
muffafa <mm.savar@gmail.com>
2024-09-14 14:47:12 +0300

    fix: type error

--- components/projects.tsx
+++ components/projects.tsx
@@ -5,6 +5,17 @@ import SectionHeading from "./section-heading";
 import { projectsData } from "@/lib/data";
 import Project from "./project";
 import { useSectionInView } from "@/lib/hooks";
+import StaticImageData from "next/image";
+
+// Add this interface above the Projects component
+interface ProjectProps {
+  title: string;
+  description: string;
+  tags: readonly string[];
+  imageUrl: string | StaticImageData;
+  link: string;
+  ribbon: { text: string; type: string } | null;
+}
 
 export default function Projects() {
   const { ref } = useSectionInView("Projects", 0.5);
